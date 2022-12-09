45de740b8c89ca4e99d112818db012594b2d18b4
Kirubel Deke <fistumkirubeldeke@gmail.com>
2022-12-08 16:44:31 -0800

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,6 +2,6 @@
 # include<studio.h>
 int main(void)
 {
-	puts(*\":programming is like developing a multilingual puzzle");
+	puts(*\":Programming is like building a multilingual puzzle");
 	return(0);
 }	
