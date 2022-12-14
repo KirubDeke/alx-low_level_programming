2af852434fbf2b0709d443ffb40fb5e2df6fbfcd
Kirubel Deke <fistumkirubeldeke@gmail.com>
2022-12-14 01:07:28 -0800

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,17 +1,9 @@
 #include "main.h"
 
-
-
 /**
- *
- *  * main - Prints "_putchar" followed by a new line.
- *
- *   *
- *
- *    * Return: Always 0.
- *
- *     */
-
+ *main - Prints _putchar followed by a new line.
+ *Return: Always 0
+ */
 int main(void)
 
 {
