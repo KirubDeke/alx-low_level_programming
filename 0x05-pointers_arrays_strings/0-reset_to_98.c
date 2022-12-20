e32cab408f027590c6d57417024146801d62faf0
Kirubel Deke <fistumkirubeldeke@gmail.com>
2022-12-20 00:30:32 -0800

    0. 98 Battery st.

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,10 +2,11 @@
 
 /**
  * restart_to_98 - update the value of n to 98
- * @n: input
- * Return: n
+ * @n: input or the integer address
+ * Return: n or 1
  */
-void reset_to_98(int *n);
+
+void reset_to_98(int *n)
 {
 	*n = 98;
 }
