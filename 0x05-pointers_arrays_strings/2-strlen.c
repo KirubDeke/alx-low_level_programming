75c7c62f96afe190b8e4ce5314ff0c1babb1de65
Kirubel Deke <fistumkirubeldeke@gmail.com>
2022-12-20 01:24:12 -0800

    2. This report, by its very length, defends itself against the risk of being read

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strlen - a function that returns the length of a string.
+ * @s: chain of character
+ * return 1 or 0
+ */
+
+int _strlen(char *s)
+{
+	int i = 0;
+
+while (s[i])
+i++;
+return (i);
+}
