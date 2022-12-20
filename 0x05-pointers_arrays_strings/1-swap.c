77bc26a272d4f7cb1e8b8dbe3ff6898dc6379bcc
Kirubel Deke <fistumkirubeldeke@gmail.com>
2022-12-20 01:05:20 -0800

    1. Don't swap horses in crossing a stream

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "math.h"
+
+/**
+ * swap int - a function that swap the values of two integers.
+ * @ a: the integer address
+ * @ b: the integer address
+ * return 1 or 0
+ */
+
+void swap_int(int *a, int *b)
+{
+	int c = *a;
+
+	*a = *b;
+        *b = *c;
+}
