5b603fba5dcfd3b73785acd96c31a3a45e121e40
KirubDeke <fistumkirubeldeke@.com>
2023-03-16 17:52:22 -0700

    5. Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
 /**
  * main - Function
- * Return: Always 0
+ * Return: Always 0(sucess)
  */
 int main(void)
 {
-	puts("\with proper grammar, but the outcome is a piece of art,);
-	Return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
