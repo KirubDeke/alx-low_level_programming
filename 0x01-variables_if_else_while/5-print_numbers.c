bb00a24a77972fab642e077a67830cabd013aeec
KirubDeke <fistumkirubeldeke@.com>
2023-03-17 12:54:49 -0700

    5. Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+/**
+ * main - a function which prints list of numbers
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int k;
+
+	for (k = 0; k < 10; k++)
+		printf("%d", k);
+
+	printf("\n");
+
+	return (0);
+}
+
