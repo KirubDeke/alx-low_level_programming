cf77b20d8e346eedc50c8bc0cda360ecad88d47b
KirubDeke <fistumkirubeldeke@.com>
2023-03-22 10:22:41 -0700

    1. Like, comment, subscribe

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * main -  causes an infinite loop
+ * main - causes an infinite loop
  * Return: 0
  */
 
@@ -10,10 +10,15 @@ int main(void)
 	int i;
 
 	printf("Infinite loop incoming :(\n");
-	i = 0;
 
+	i = 0;
+/*
+ * while (i < 10)
+ * {
+ * putchar(i);
+ * }
+ */
 	printf("Infinite loop avoided! \\o/\n");
 
- 
 	return (0);
 }
