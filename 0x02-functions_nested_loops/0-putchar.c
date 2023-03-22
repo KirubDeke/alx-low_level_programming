566c89eab4ab4b658e2021c727fd73378df1a0a0
KirubDeke <fistumkirubeldeke@.com>
2023-03-22 02:49:23 -0700

    0-putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,6 +8,7 @@
 
 int main(void)
 {
+
 	_putchar('_');
 	_putchar('p');
 	_putchar('u');
@@ -16,8 +17,9 @@ int main(void)
 	_putchar('h');
 	_putchar('a');
 	_putchar('r');
-	_putchar('\n');
-    
+        _putchar('\n');
 	return (0);
 
+
 }
+
