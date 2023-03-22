7f749e39b7be08978a2bd89fcd7e33efcefefb15
KirubDeke <fistumkirubeldeke@.com>
2023-03-22 10:53:43 -0700

    1. I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -1,1 +1,1 @@
-This a read me file for functions nested loops
+README
--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -2,14 +2,18 @@
 
 /**
  * _putchar - writes the character c to stdout
- *  @c: The character to print
+ * @c: The character to print
  *
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
 
-int _putchar(char c)
+int main(void)
 {
-	return (write(1, &c, 1));
-}
+	int i;
 
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
