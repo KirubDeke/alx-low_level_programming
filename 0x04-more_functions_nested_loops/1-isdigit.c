88fef42f1da5ca667a86ab22152c2b543ea631a6
KirubDeke <fistumkirubeldeke@.com>
2023-03-24 11:10:11 -0700

    1. isdigit

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -4,10 +4,10 @@
  *  @c: a variable input to be checked
  *  Return: 1 if it is a digit or 0 if it is not
  */
-int _isdigit(int c);
+int _isdigit(int c)
 {
 	if((c >= 0) &&(c <= 10))
 		return (1);
 	else
 		return (0);
-
+}
