58fb5c634ff5a6b76743e0c4e15e412710efbc9d
KirubDeke <fistumkirubeldeke@.com>
2023-03-29 14:22:18 -0700

    4. I am a kind of paranoid in reverse. I suspect people of plotting to make me happy

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,30 +1,26 @@
-void print_array(int *a, int n)
-{
-    int i;
-
-    i = 0;
-    while (i < n)
-    {
-        if (i != 0)
-        {
-            printf(", ");
-        }
-        printf("%d", a[i]);
-        i++;
-    }
-    printf("\n");
-}
+#include "main.h"
 
 /**
- * main - check the code
+ * reverse_array - Write a function that reverses the content of an array of
+ * integers.
  *
- * Return: Always 0.
+ * @a: This is the input array
+ * @n: This is the positions have the array
+ *
  */
-int main(void)
+void reverse_array(int *a, int n)
 {
-    int a[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 98, 1024, 1337};
+	int i, last;
 
-    print_array(a, sizeof(a) / sizeof(int));
-    reverse_array(a, sizeof(a) / sizeof(int));
-    print_array(a, sizeof(a) / sizeof(int));
-  
+	last = n - 1;
+	for (i = 0; i < n / 2; i++)
+	{
+		int start, end;
+
+		start = a[i];
+		end = a[last];
+		a[i] = end;
+		a[last] = start;
+		last--;
+	}
+} 
